6d7c3439f2278805c2623bf7cf8d24a181a00d4a
adamrichardturner <adamrichardturner@hotmail.com>
2019-04-06 08:07:16 +0100

    initial upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* jshint esversion:6 */
+
+let compass = document.getElementById("compass"),
+  eye = document.getElementById("eye"),
+  edit = document.getElementById("edit"),
+  polygon = document.getElementById("polygon"),
+  projects = document.getElementById("projects");
+
+let inview = new Waypoint.Inview({
+  element: $('#about--icons')[0],
+  entered: function(direction) {
+    compass.style.transform = "rotate(360deg)";
+    eye.style.transform = "rotate(360deg)";
+    edit.style.transform = "rotate(360deg)";
+    polygon.style.transform = "rotate(360deg)";
+    projects.style.transform = "animation-name:moveInLeft";
+    projects.style.transform = "animation-duration:1s";
+    projects.style.transform = "animation-timing-function:ease-out";
+    projects.style.transform = "animation-delay:2s";
+  }
+});
