b5b4f28594fa6ab56dab41ab89a0cbe96d420758
adamrichardturner <adamrichardturner@hotmail.com>
2019-04-08 21:57:31 +0100

    update html

--- js/script.js
+++ js/script.js
@@ -1,25 +1,5 @@
 /* jshint esversion:6 */
 
-// let compass = document.getElementById("compass"),
-//   eye = document.getElementById("eye"),
-//   edit = document.getElementById("edit"),
-//   polygon = document.getElementById("polygon"),
-//   projects = document.getElementById("projects");
-//
-// let inview = new Waypoint.Inview({
-//   element: $('#about--icons')[0],
-//   entered: function(direction) {
-//     compass.style.transform = "rotate(360deg)";
-//     eye.style.transform = "rotate(360deg)";
-//     edit.style.transform = "rotate(360deg)";
-//     polygon.style.transform = "rotate(360deg)";
-//     projects.style.transform = "animation-name:moveInLeft";
-//     projects.style.transform = "animation-duration:1s";
-//     projects.style.transform = "animation-timing-function:ease-out";
-//     projects.style.transform = "animation-delay:2s";
-//   }
-// });
-
 $(document).ready(function() {
   $('#header-trigger').waypoint(function() {
     $('#about').addClass('animated fadeInLeft');
@@ -31,7 +11,7 @@ $(document).ready(function() {
   }, {
     offset: '50%'
   });
-  $('#contact').waypoint(function() {
+  $('#contact-trigger').waypoint(function() {
     $('#contact').addClass('animated fadeInLeft');
   }, {
     offset: '50%'
