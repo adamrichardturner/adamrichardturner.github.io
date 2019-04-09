1bfbbdea2474e8554d295fb833e3295fcc5ec1f2
adamrichardturner <adamrichardturner@hotmail.com>
2019-04-09 11:51:27 +0100

    update stuff

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 /* jshint esversion:6 */
-
 $(document).ready(function() {
-  $('#header-trigger').waypoint(function() {
-    $('#about').addClass('animated fadeInLeft');
-  }, {
-    offset: '50%'
-  });
   $('#projects-trigger').waypoint(function() {
     $('#projects').addClass('animated fadeInRight');
   }, {
